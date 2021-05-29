92518bcc8f0c0a9ce7d1cc1be386f5b874a7d4d2
Andrii Baranchuk <andrii.baranchuk@meliorgames.com>
2021-05-29 23:36:12 +0300

    Added HeroHealth, HpBar.

---
+++ Assets/Scripts/HeroHealth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroHealth : MonoBehaviour
+{
+    public float Max;
+    public float Current;
+
+    public event Action Changed;
+
+    private void Awake()
+    {
+        Current = Max;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(Current <= 0)
+            return;
+
+        Current -= damage;
+        
+        Changed?.Invoke();
+    }
+}
---
+++ Assets/Scripts/HpBar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+  public Image ImageCurrent;
+
+  public HeroHealth Health;
+
+  private void Start()
+  {
+    Health.Changed += UpdateHpBar;
+  }
+
+  private void OnDestroy()
+  {
+    Health.Changed -= UpdateHpBar;
+  }
+
+  private void UpdateHpBar()
+  {
+    SetValue(Health.Current, Health.Max);
+  }
+
+  private void SetValue(float current, float max)
+  {
+    ImageCurrent.fillAmount = current / max;
+  }
+}
